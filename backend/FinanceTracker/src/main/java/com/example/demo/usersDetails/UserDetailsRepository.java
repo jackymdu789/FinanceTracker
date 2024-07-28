017a94fac241066ca54a088b047b1bbf88e914d7
pradeep <pradeep@gmail.com>
2024-07-28 06:27:39 +0000

    add user Repository class

---
+++ backend/FinanceTracker/src/main/java/com/example/demo/usersDetails/UserDetailsRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.usersDetails;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserDetailsRepository extends CrudRepository<UserDetails, Integer> {
+
+}
