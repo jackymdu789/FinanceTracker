a1cb3d331dba17163211c327aad70058875641e8
Amulya2706 <amulyeah47@gmail.com>
2024-07-29 05:03:53 +0000

    small change in commit

--- backend/FinanceTracker/src/main/java/com/example/demo/account/AccountServices.java
+++ backend/FinanceTracker/src/main/java/com/example/demo/account/AccountServices.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class AccountServices {
 	
 	@Autowired
-	AccountRespository respo;
+	AccountRepository respo;
 	
 	Iterable<Account> getAllAccountDetails() {
 		return respo.findAll();
