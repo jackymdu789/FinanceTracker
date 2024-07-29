d461a5bb9ac6b090b00f7d5212271911e1e4110c
Amulya2706 <amulyeah47@gmail.com>
2024-07-29 04:51:59 +0000

    new commit

--- backend/FinanceTracker/src/main/java/com/example/demo/account/AccountController.java
+++ backend/FinanceTracker/src/main/java/com/example/demo/account/AccountController.java
@@ -3,10 +3,12 @@ package com.example.demo.account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("/api/v1/account")
 public class AccountController {
 	
 	@Autowired
